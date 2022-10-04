8d9b2ef7e1ebc7e2bb483e79138399c1bb640bff
eliasbokreta <elias.bokreta@gmail.com>
2022-10-04 19:13:47 +0200

    chore: load deprecated resources file definition with config

--- pkg/utils/config.go
+++ pkg/utils/config.go
@@ -9,12 +9,12 @@ import (
 // Load configuration file
 func LoadConfig() error {
 	viper.AddConfigPath("./config")
+	viper.AddConfigPath("$HOME/.deprek8")
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 
 	// Set configuration default values
 	viper.SetDefault("deprek8.helm.repository.artifacthub.apiBaseUrl", "https://artifacthub.io/api/v1")
-	viper.SetDefault("deprek8.helm.resourcesFile", "./config/api.yaml")
 
 	if err := viper.ReadInConfig(); err != nil {
 		return fmt.Errorf("could not retrieve config file: %w", err)
